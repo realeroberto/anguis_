2f2b3e11c305e79c6bebff07c09165331a58931f
Roberto Reale <rober.reale@gmail.com>
2018-02-16 19:28:01 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'anguis',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A generic key-store library',
     packages = [ 'anguis' ],
     author = 'Roberto Reale',
