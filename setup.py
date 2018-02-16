ff941e06284edea1000ee72de23cf8903647887f
Roberto Reale <rober.reale@gmail.com>
2018-02-16 10:24:35 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'anguis',
+    version = '0.0.1',
+    description = 'A generic key-store library',
+    packages = [ 'anguis' ],
+    author = 'Roberto Reale',
+    author_email = 'rober.reale@gmail.com',
+    url = 'https://github.com/robertoreale/anguis',
+    keywords = [ , ],
+    install_requires = [ ]
+)
