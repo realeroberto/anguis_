d879043ca08d0ffa77a3dfcaae825fa4a97a9dfc
Roberto Reale <rober.reale@gmail.com>
2018-02-19 18:41:46 +0100

    Add unit test for SFTP.

--- anguis/tests/test.py
+++ anguis/tests/test.py
@@ -1,6 +1,7 @@
+import os
 from unittest import TestCase
 
-from anguis import anguisEtcd, anguisFS, anguisGit, anguisRedis
+from anguis import anguisEtcd, anguisFS, anguisGit, anguisRedis, anguisSFTP
 
 class Test(TestCase):
     def testFS(self):
@@ -34,5 +35,19 @@ class Test(TestCase):
         print(cache.get('foo'))
 
         self.assertTrue(cache.get('foo') == 'bar')
+
+    def testSFTP(self):
+        hostname = os.environ['SFTP_HOSTNAME']
+        username = os.environ['SFTP_USERNAME']
+        password = os.environ['SFTP_PASSWORD']
+        dir = os.environ['SFTP_DIR']
+
+        cache = anguisSFTP.AnguisSFTP(hostname, username, password, dir)
+
+        cache.set('foo', 'bar')
+        print(cache.get('foo'))
+
+        self.assertTrue(cache.get('foo') == 'bar')
+
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
