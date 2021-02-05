3860a6e48d29032ed3162f611f5389168e0cb792
Roberto Reale <roberto@reale.me>
2021-02-05 01:57:36 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/reale/anguis',
-    version='0.3.5',
+    version='0.3.6',
     zip_safe=False,
 )
 
